20020ecde9ed1b51bb549354e268a3d7cfda40f6
DiptiLecturer <lecturer3.amad.nsda@dipti.com.bd>
2026-02-16 10:03:42 +0600

    "done"

--- app/src/main/java/org/freedu/basicsharedpreferenceb7/MainActivity.kt
+++ app/src/main/java/org/freedu/basicsharedpreferenceb7/MainActivity.kt
@@ -1,9 +1,13 @@
 package org.freedu.basicsharedpreferenceb7
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -12,62 +16,80 @@ import org.freedu.basicsharedpreferenceb7.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var sharedPref: SharedPreferences
-    private lateinit var adapter: NoteAdapter
+
     private val noteList = mutableListOf<Note>()
+    private lateinit var adapter: NoteAdapter
 
-
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         sharedPref = getSharedPreferences("notes_pref", MODE_PRIVATE)
 
-        adapter= NoteAdapter(noteList)
+        adapter = NoteAdapter(noteList)
+
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter=adapter
 
+        binding.recyclerView.adapter = adapter
 
+        // Load saved text into EditText
+        binding.editNote.setText(sharedPref.getString("draft_text", ""))
 
-        //load data
+        binding.editNote.addTextChangedListener {
+            sharedPref.edit { putString("draft_text", it.toString()) }
+        }
+
         loadNotes()
 
 
-        binding.btnSave.setOnClickListener {
-            val note = binding.editNote.text.toString()
+        // FAB Click
+        binding.btnAdd.setOnClickListener {
 
-            if(note.isNotEmpty()){
-                noteList.add(Note(note))
-                saveNote()
+            val text = binding.editNote.text.toString()
+
+            if (text.isNotEmpty()) {
+
+                noteList.add(Note(text))
+
+                saveNotes()
+
                 adapter.notifyDataSetChanged()
-                binding.editNote.text.clear()
+
+                binding.editNote.text?.clear()
+
+                sharedPref.edit { remove("draft_text") }
             }
         }
-
     }
-    //save the data
-    fun saveNote(){
+
+    private fun saveNotes() {
+
         val gson = Gson()
         val json = gson.toJson(noteList)
 
-        sharedPref.edit()
-            .putString("notes",json)
-            .apply()
+        sharedPref.edit {
+            putString("notes", json)
+        }
     }
-    //load the data
 
     private fun loadNotes() {
 
         val gson = Gson()
-
-        val json = sharedPref.getString("notes",null)
+        val json = sharedPref.getString("notes", null)
 
         val type = object : TypeToken<MutableList<Note>>() {}.type
 
-        val savedList: MutableList<Note>? = gson.fromJson(json,type)
+        val savedList: MutableList<Note>? = gson.fromJson(json, type)
 
-        if(savedList!=null){
+        if (savedList != null) {
             noteList.addAll(savedList)
         }
     }
